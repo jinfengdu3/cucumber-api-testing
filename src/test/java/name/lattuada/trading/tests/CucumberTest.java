b383c4502a6e4ecd0979edda159555b0e975300d
Jinfeng Du <jinfengdu@Jinfengs-MacBook-Pro.local>
2024-04-23 00:47:20 +0200

    add support for Extent report and cucumber report to generate test report

--- src/test/java/name/lattuada/trading/tests/CucumberTest.java
+++ src/test/java/name/lattuada/trading/tests/CucumberTest.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"},
-        features = "src/test/resources")
+@CucumberOptions(plugin = { "pretty", "html:target/cucumber-report.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/extent-reports" }, 
+                features = "src/test/resources", 
+                glue = "name.lattuada.trading.tests")
 @CucumberContextConfiguration
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class CucumberTest {
 }
