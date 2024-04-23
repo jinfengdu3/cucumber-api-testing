336f60fa2a02b3342a20f326203992887d551c17
Jinfeng Du <jinfengdu@Jinfengs-MacBook-Pro.local>
2024-04-23 11:55:26 +0200

    move the feature file to seperate feature folder to ensure the readability and scalability

--- src/test/java/name/lattuada/trading/tests/CucumberTest.java
+++ src/test/java/name/lattuada/trading/tests/CucumberTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "html:target/cucumber-report.html",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/extent-reports" }, 
-                features = "src/test/resources", 
+                features = "src/test/resources/features",
                 glue = "name.lattuada.trading.tests")
 @CucumberContextConfiguration
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
